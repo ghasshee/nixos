e436f4f717709f3ec450da6920347c5d17ef0caf
ghasshee <ghasshee@gmail.com>
2018-07-20 19:09:53 +0900

    Updated

--- install.zsh
+++ install.zsh
@@ -3,4 +3,4 @@
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration-old.nix
 sudo ln -s $HOME/ghasshee/nixos/configuration.nix /etc/nixos/configuration.nix
 if [[ -d ~/.config ]] ; then mv ~/.config ~/.config-old ; fi
-ln -s $HOME/ghasshee/nixos/.config/ ~/.config
+cp -r $HOME/ghasshee/nixos/.config/ ~/.config
