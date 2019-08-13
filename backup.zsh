cf760ad615edc6b9292a0af96690f5cd54469e05
ghasshee <ghasshee@gmail.com>
2019-08-14 03:13:20 +0900

    Updated

--- backup.zsh
+++
@@ -1,3 +0,0 @@
-cp /etc/nixos/configuration.nix ~/ghasshee/nixos
-cp /etc/nixos/packages.nix      ~/ghasshee/nixos
-
--- install_config.zsh
+++
@@ -1,3 +0,0 @@
-sudo cp $HOME/ghasshee/nixos/configuration.nix /etc/nixos/configuration.nix
-if [[ -d ~/.config ]] ; then mv ~/.config ~/.config-old ; fi
-cp -r $HOME/ghasshee/nixos/.config/ ~/.config
